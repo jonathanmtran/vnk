bc5bc45e7bdaa39405820f00b7f3c3c9ba5fea40
Jonathan Tran <jonathanmtran@users.noreply.github.com>
2023-04-10 23:54:13 -0700

    refactor resolvers out into its own file and implemented YouTube search

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,36 +1,28 @@
-import { ApolloServer } from '@apollo/server';
-import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { readFileSync } from 'fs';
-import { NextRequest } from 'next/server';
-import knextfile from "../../knexfile";
-import { Resolvers } from '../../src/__generated__/resolvers-types';
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { readFileSync } from "fs";
+import { NextRequest } from "next/server";
+import resolvers from "../../src/resolvers";
+import { YouTubeAPI } from "../../src/youTubeAPI";
 
-const db = require("knex")(knextfile);
+const typeDefs = readFileSync("./schema.graphql", { encoding: "utf-8" });
 
-const resolvers: Resolvers = {
-  Query: {
-    queues: async () => {
-      const resultset = await db("queues")
-        .select();
+export interface MyContext {
+  dataSources: {
+    youTubeApi: YouTubeAPI;
+  };
+}
 
-      return resultset.map((row: any) => {
-        return {
-          id: row.id,
-          name: row.queue_name,
-          created: row.created,
-        }
-      });
-    }
-  },
-};
-
-const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' });
-
-const server = new ApolloServer({
+const server = new ApolloServer<MyContext>({
   resolvers,
   typeDefs,
 });
 
 export default startServerAndCreateNextHandler<NextRequest>(server, {
-  context: async req => ({ req })
+  context: async (req) => ({
+    req,
+    dataSources: {
+      youTubeApi: new YouTubeAPI(),
+    },
+  }),
 });
