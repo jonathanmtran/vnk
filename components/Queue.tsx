a8d39191556c5028c09062838d5f2c99e1248b34
Jonathan Tran <jonathanmtran@users.noreply.github.com>
2023-04-27 01:47:24 -0700

    Reimplemented Queue as functional component

--- components/Queue.tsx
+++ components/Queue.tsx
@@ -7,77 +7,45 @@ import {
   TabPanels,
   Tabs,
 } from "@chakra-ui/react";
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import QueueEntry from "./QueueEntry";
 
 type QueueProps = {
   id: string;
 };
 
-type QueueState = {
-  queueId: string;
-  queue: Array<any>;
-  upcoming: Array<any>;
-  performed: Array<any>;
-};
+export default function Queue(props: QueueProps) {
+  const queueId = props.id;
+  const [queue, setQueue] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [performed, setPerformed] = useState([]);
 
-export default class Queue extends Component<QueueProps, QueueState> {
-  constructor(props: QueueProps) {
-    super(props);
+  useEffect(() => {
+    fetchData(queueId);
+  }, [queueId]);
 
-    this.state = {
-      queueId: this.props.id,
-      queue: [],
-      upcoming: [],
-      performed: [],
-    };
-
-    this.componentDidUpdate = this.componentDidUpdate.bind(this);
-    this.handlePerform = this.handlePerform.bind(this);
-    this.handleRemove = this.handleRemove.bind(this);
-  }
-
-  componentDidMount() {
-    if (!this.props.id && !this.state.queueId) {
-      return;
-    }
-    this.fetchData(this.props.id);
-  }
-
-  /* eslint-disable no-unused-vars */
-  componentDidUpdate(prevProps: any, prevState: any, snapshot: any) {
-    if (prevProps.id !== this.props.id) {
-      this.setState({
-        queueId: this.props.id,
-      });
-      this.fetchData(this.props.id);
-    }
-  }
-
-  fetchData(id: string) {
+  const fetchData = (id: string) => {
     if (typeof id === "undefined") return;
-
+    console.debug({ ctx: "fetchData" });
     fetch(`/api/queue/${id}`)
       .then((response) => response.json())
       .then((json) => {
-        let upcoming = json.queue.filter((item: any) => {
+        const upcoming = json.queue.filter((item: any) => {
           return item.performed === null;
         });
 
-        let performed = json.queue.filter((item: any) => {
+        const performed = json.queue.filter((item: any) => {
           return item.performed !== null;
         });
 
-        this.setState({
-          queue: json.queue,
-          upcoming: upcoming,
-          performed: performed,
-        });
+        setQueue(json.queue);
+        setUpcoming(upcoming);
+        setPerformed(performed);
       });
-  }
+  };
 
-  handleRemove(id: string) {
-    fetch(`/api/queue/${this.state.queueId}`, {
+  const handleRemove = (id: string) => {
+    fetch(`/api/queue/${queueId}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -88,23 +56,21 @@ export default class Queue extends Component<QueueProps, QueueState> {
     })
       .then((response) => response.json())
       .then((json) => {
-        let updatedQueue = this.state.queue.filter((item) => {
+        const updatedQueue = queue.filter((item: any) => {
           return item.id !== json.id;
         });
 
-        let updatedUpcomingQueue = this.state.upcoming.filter((item) => {
+        const updatedUpcomingQueue = upcoming.filter((item: any) => {
           return item.id !== json.id;
         });
 
-        this.setState({
-          queue: updatedQueue,
-          upcoming: updatedUpcomingQueue,
-        });
+        setQueue(updatedQueue);
+        setUpcoming(updatedUpcomingQueue);
       });
-  }
+  };
 
-  handlePerform(id: string) {
-    fetch(`/api/queue/${this.state.queueId}`, {
+  const handlePerform = (id: string) => {
+    fetch(`/api/queue/${queueId}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
@@ -115,50 +81,44 @@ export default class Queue extends Component<QueueProps, QueueState> {
     })
       .then((response) => response.json())
       .then((jsonResponse) => {
-        if (jsonResponse.id !== this.state.queueId) {
-          console.error({ queueId: this.state.queueId, id: jsonResponse.id });
+        if (jsonResponse.id !== queueId) {
+          console.error({ queueId: queueId, id: jsonResponse.id });
         }
 
-        this.fetchData(this.state.queueId);
+        fetchData(queueId);
       });
-  }
+  };
 
-  render() {
-    return (
-      <>
-        <Heading as="h3">Queue</Heading>
+  return (
+    <>
+      <Heading as="h3">Queue</Heading>
 
-        <Tabs variant="enclosed">
-          <TabList>
-            <Tab>
-              Upcoming <Badge>{this.state.upcoming.length}</Badge>
-            </Tab>
-            <Tab>Performed</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              {this.state.upcoming.length === 0 ? "The queue is empty" : null}
-              {this.state.upcoming.map((item) => (
-                <QueueEntry
-                  key={item.id}
-                  {...item}
-                  onPerform={this.handlePerform}
-                  onRemove={this.handleRemove}
-                />
-              ))}
-            </TabPanel>
-            <TabPanel>
-              {this.state.performed.map((item) => (
-                <QueueEntry
-                  key={item.id}
-                  {...item}
-                  onPerform={this.handlePerform}
-                />
-              ))}
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </>
-    );
-  }
+      <Tabs variant="enclosed">
+        <TabList>
+          <Tab>
+            Upcoming <Badge>{upcoming.length}</Badge>
+          </Tab>
+          <Tab>Performed</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            {upcoming.length === 0 ? "The queue is empty" : null}
+            {upcoming.map((item: any) => (
+              <QueueEntry
+                key={item.id}
+                {...item}
+                onPerform={handlePerform}
+                onRemove={handleRemove}
+              />
+            ))}
+          </TabPanel>
+          <TabPanel>
+            {performed.map((item: any) => (
+              <QueueEntry key={item.id} {...item} onPerform={handlePerform} />
+            ))}
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
+  );
 }
